6f10f15e5abce8e6a6577a275ea2bd046d032add
luanafao <lulu.assis10@gmail.com>
2026-06-06 17:35:48 -0300

    feat: add initial output messages to main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+
 int main() {
+    std::cout << "==================================" << std::endl;
+    std::cout << "Edu Social Backend iniciado!" << std::endl;
+    std::cout << "Pressione ENTER para encerrar..." << std::endl;
+    std::cout << "==================================" << std::endl;
+
+    std::cin.get();
+
     return 0;
 }
