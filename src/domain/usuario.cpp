df854049b81ecca7e668ad4a7023e09d41985276
Milena <milenaflaviacs@gmail.com>
2026-06-08 20:40:25 -0300

    refactor: improve CMake configuration and update Usuario class implementation

--- src/domain/usuario.cpp
+++ src/domain/usuario.cpp
@@ -1,13 +1,24 @@
 #include "domain/usuario.hpp"
 
-Usuario::Usuario(std::string email, std::string senha, std::string nomeDeUsuario)
-    : senha(std::move(senha)), email(std::move(email)), nomeDeUsuario(std::move(nomeDeUsuario)) {}
+#include <utility>
+
+Usuario::Usuario(
+    std::string emailInicial,
+    std::string senhaInicial,
+    std::string nomeInicial
+)
+    : senha(std::move(senhaInicial)),
+      email(std::move(emailInicial)),
+      nomeDeUsuario(std::move(nomeInicial))
+{
+}
 
 bool Usuario::alterarSenha(std::string senhaAtual, std::string novaSenha)
 {
     if (senhaAtual != senha) {
         return false;
     }
+
     senha = std::move(novaSenha);
     return true;
 }
@@ -17,13 +28,17 @@ bool Usuario::alterarEmail(std::string senhaAtual, std::string novoEmail)
     if (senhaAtual != senha) {
         return false;
     }
+
     email = std::move(novoEmail);
     return true;
 }
 
-bool Usuario::fazerLogin(std::string email, std::string senha)
+bool Usuario::fazerLogin(
+    std::string emailInformado,
+    std::string senhaInformada
+)
 {
-    return email == this->email && senha == this->senha;
+    return emailInformado == email && senhaInformada == senha;
 }
 
 std::string Usuario::getEmail()
