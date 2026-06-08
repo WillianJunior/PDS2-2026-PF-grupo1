cbb5d5b5ecad7f048264041c13c49c14f411fc0c
joao-sirge <joao.sirge@gmail.com>
2026-06-08 08:38:40 -0300

    Update post.cpp

--- src/domain/post.cpp
+++ src/domain/post.cpp
@@ -1,34 +1,19 @@
-#include "domain/post.hpp"
-#include <utility>
+#include "post.hpp"
+#include <algorithm>
 
-Post::Post(std::string texto, int idAutor)
-    : texto(std::move(texto)), idsCurtidas(), idAutor(idAutor), comentarios() {}
-
-void Post::adicionarComentario(Comentario comentario) {
-    comentarios.push_back(std::move(comentario));
-}
-
-std::vector<Comentario> Post::listarComentarios() {
-    return comentarios;
-}
-
-int Post::quantidadeDeCurtidas() {
-    return static_cast<int>(idsCurtidas.size());
-}
+Post::Post(int id, int idAutor, int idComunidade, std::string texto)
+    : id(id), idAutor(idAutor), idComunidade(idComunidade), texto(std::move(texto)) {}
 
 void Post::curtir(int idUsuario) {
-    auto it = idsCurtidas.find(idUsuario);
-    if (it == idsCurtidas.end()) {
-        idsCurtidas.insert(idUsuario);
-    } else {
-        idsCurtidas.erase(it);
-    }
+    auto it = std::find(idsCurtidas.begin(), idsCurtidas.end(), idUsuario);
+    if (it == idsCurtidas.end()) idsCurtidas.push_back(idUsuario);
+    else idsCurtidas.erase(it);
 }
 
-std::string Post::getConteudo() const {
-    return texto;
-}
-
-std::vector<int> Post::getIdsCurtidas() const {
-    return std::vector<int>(idsCurtidas.begin(), idsCurtidas.end());
-}
+int Post::quantidadeDeCurtidas() const { return idsCurtidas.size(); }
+int Post::getId() const { return id; }
+int Post::getIdAutor() const { return idAutor; }
+int Post::getIdComunidade() const { return idComunidade; }
+std::string Post::getConteudo() const { return texto; }
+std::vector<int> Post::getIdsCurtidas() const { return idsCurtidas; }
+void Post::setIdsCurtidas(const std::vector<int>& ids) { idsCurtidas = ids; }
