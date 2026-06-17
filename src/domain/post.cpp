9f9eb028562cf92fdba329cf0a74ea31af401c3e
luanafao <lulu.assis10@gmail.com>
2026-06-17 18:54:45 -0300

    refactor: improve Post class formatting and return types for better const correctness

--- src/domain/post.cpp
+++ src/domain/post.cpp
@@ -2,30 +2,60 @@
 #include <algorithm>
 
 Post::Post(int id, int idAutor, int idComunidade, std::string texto)
-    : id(id), idAutor(idAutor), idComunidade(idComunidade), texto(std::move(texto)) {}
+    : id(id),
+      idAutor(idAutor),
+      idComunidade(idComunidade),
+      texto(std::move(texto)) {}
 
 Post::Post(std::string texto, int idAutor)
-    : id(0), idAutor(idAutor), idComunidade(0), texto(std::move(texto)) {}
+    : id(0),
+      idAutor(idAutor),
+      idComunidade(0),
+      texto(std::move(texto)) {}
 
 void Post::curtir(int idUsuario) {
     auto it = std::find(idsCurtidas.begin(), idsCurtidas.end(), idUsuario);
-    if (it == idsCurtidas.end()) idsCurtidas.push_back(idUsuario);
-    else idsCurtidas.erase(it);
+    if (it == idsCurtidas.end())
+        idsCurtidas.push_back(idUsuario);
+    else
+        idsCurtidas.erase(it);
 }
 
-int Post::quantidadeDeCurtidas() const { return idsCurtidas.size(); }
+int Post::quantidadeDeCurtidas() const {
+    return idsCurtidas.size();
+}
 
 void Post::adicionarComentario(const Comentario& c) {
     comentariosTestes.push_back(c);
 }
 
-std::vector<Comentario> Post::listarComentarios() const {
+
+const std::vector<Comentario>& Post::listarComentarios() const {
     return comentariosTestes;
 }
 
-int Post::getId() const { return id; }
-int Post::getIdAutor() const { return idAutor; }
-int Post::getIdComunidade() const { return idComunidade; }
-std::string Post::getConteudo() const { return texto; }
-std::vector<int> Post::getIdsCurtidas() const { return idsCurtidas; }
-void Post::setIdsCurtidas(const std::vector<int>& ids) { idsCurtidas = ids; }
+int Post::getId() const {
+    return id;
+}
+
+int Post::getIdAutor() const {
+    return idAutor;
+}
+
+int Post::getIdComunidade() const {
+    return idComunidade;
+}
+
+
+const std::string& Post::getConteudo() const {
+    return texto;
+}
+
+
+const std::vector<int>& Post::getIdsCurtidas() const {
+    return idsCurtidas;
+}
+
+void Post::setIdsCurtidas(const std::vector<int>& ids) {
+    idsCurtidas = ids;
+}
